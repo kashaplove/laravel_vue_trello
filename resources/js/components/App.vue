50f1c08320feefc5bc474390e2af62e63292a0c3
Roman <roma20.94.a@gmail.com>
2022-06-19 09:41:17 +0300

    add vue-router

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,12 +1,18 @@
 <template>
     <div class="container">
-        <h1>Hello world</h1>
+        <p>
+            <router-link :to="{name: 'home'}">Home</router-link>
+            <router-link :to="{name: 'about'}">About</router-link>
+        </p>
+        <div>
+            <router-view></router-view>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: "App"
+
 }
 </script>
 
